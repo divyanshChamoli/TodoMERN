801a1a6bb62af7a3ebae915271cb778a70dac223
Divyansh Chamoli <divyanshchamoli567@gmail.com>
2025-03-06 15:23:21 +0530

    bugfix: fixed add todo bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import AddTodo from "./Components/AddTodo"
 import RenderTodo from "./Components/RenderTodo"
 
 function App() {
+  const [todos,setTodos]=useState([])
   return (
     <>
-      <AddTodo/>
-      <RenderTodo/>
+      <AddTodo todos={todos} setTodos={setTodos} />
+      <RenderTodo todos={todos} setTodos={setTodos} />
     </>
   )
 }
